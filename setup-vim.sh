aa67088fad212406e2bc910f0aeff480146ffa78
antoine drouhin <antoine.drouhin@gmail.com>
2020-01-20 12:04:09 +0100

    [vim] Add an install for various vim tools

--- setup-vim.sh
+++ setup-vim.sh
@@ -1,11 +1,34 @@
-## Support typescript
 
-git clone https://github.com/leafgarland/typescript-vim.git ~/.vim/pack/typescript/start/typescript-vim
 
-## color 
+##########################################################
+## Setupt vim dependencies
+## Use native package 
+##########################################################
+
+
+# COLORS 
 
 mkdir -p ~/.vim/colors
 
 curl https://raw.githubusercontent.com/ayu-theme/ayu-vim/master/colors/ayu.vim > ~/.vim/colors/ayu.vim
 curl https://raw.githubusercontent.com/Mcmartelle/vim-monokai-bold/master/colors/monokai-bold.vim > ~/.vim/colors/monokai-bold.vim
+
+# FEATURES
+
+## Code intelligence
+
+git clone -b release https://github.com/neoclide/coc.nvim.git ~/.vim/pack/coc/start/coc
+
+
+# LANGUAGES
+
+## Typescript
+
+git clone https://github.com/leafgarland/typescript-vim.git ~/.vim/pack/typescript/start/typescript-vim
+
+## Vim Plug
+
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
 
